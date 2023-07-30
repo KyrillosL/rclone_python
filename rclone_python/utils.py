066f25bf68bcc2b87c27260fcaadba537b9255ff
Cyril <lepinette.cyril@gmail.com>
2023-07-30 22:55:48 +0200

    [WIP/NeedRefactor] Display check progess

--- rclone_python/utils.py
+++ rclone_python/utils.py
@@ -12,11 +12,34 @@ from rich.progress import (
     TimeRemainingColumn,
     SpinnerColumn,
     DownloadColumn,
+    MofNCompleteColumn
 )
 
 # ---------------------------------------------------------------------------- #
 #                               General Functions                              #
 # ---------------------------------------------------------------------------- #
+
+class MyProgress(Progress):
+    def get_renderables(self):
+        for task in self.tasks:
+            if task.fields.get("progress_type") == "checking":
+                self.columns = (
+                    TextColumn("[progress.description]{task.description}"),
+                    SpinnerColumn(),
+                    MofNCompleteColumn(),
+                    SpinnerColumn()
+                )
+
+            if task.fields.get("progress_type") == "download":
+                self.columns = (
+                    TextColumn("[progress.description]{task.description}"),
+                    SpinnerColumn(),
+                    BarColumn(),
+                    TaskProgressColumn(),
+                    DownloadColumn(binary_units=True),
+                    TimeRemainingColumn(),
+                )
+            yield self.make_tasks_table([task])
 
 
 def args2string(args: List[str]) -> str:
@@ -101,7 +124,7 @@ def rclone_progress(
     subprocesses = {}
 
     if show_progress:
-        pbar, total_progress_id = create_progress_bar(pbar_title)
+        pbar, total_progress_id, total_progress_check = create_progress_bar(pbar_title)
 
     process = subprocess.Popen(
         command, stdout=subprocess.PIPE, stderr=stderr, shell=True
@@ -111,10 +134,23 @@ def rclone_progress(
 
         valid, update_dict = extract_rclone_progress(buffer)
 
+        valid_check, update_dict_check = extract_rclone_progress_check(buffer)
+
+        if valid_check:
+            if show_progress:
+                pbar.update(
+                    total_progress_check,
+                    completed=update_dict_check["processed"] -1, #so it's not always 100%
+                    total=update_dict_check["nb_files"],
+                    #description= "Total : " + str(update_dict_check["nb_files"])
+                )
+
+
         if valid:
             if show_progress:
                 update_tasks(pbar, total_progress_id, update_dict, subprocesses)
 
+        if valid or valid_check:
             # call the listener
             if listener:
                 listener(update_dict)
@@ -133,7 +169,6 @@ def rclone_progress(
         pbar.stop()
 
     return process
-
 
 def extract_rclone_progress(buffer: str) -> Tuple[bool, Union[Dict[str, Any], None]]:
     # matcher that checks if the progress update block is completely buffered yet (defines start and stop)
@@ -175,6 +210,33 @@ def extract_rclone_progress(buffer: str) -> Tuple[bool, Union[Dict[str, Any], No
         )[0]
         out["eta"] = eta
 
+        return True, out,
+
+    else:
+        return False, None
+
+def extract_rclone_progress_check(buffer: str) -> Tuple[bool, Union[Dict[str, Any], None]]:
+    # matcher that checks if the progress update block is completely buffered yet (defines start and stop)
+    # it gets the sent bits, total bits, progress, transfer-speed and eta
+
+    reg_checked = re.findall(
+        r"Checks:\s+(\d+) \/ (\d+), (\d{1,3})%",
+        buffer,
+    )
+
+    if reg_checked:  # transferred block is completely buffered
+        # get the progress of the individual files
+        # matcher gets the currently transferring files and their individual progress
+        # returns list of tuples: (name, progress, file_size, unit)
+        prog_check = []
+        prog_regex = re.findall(
+            r"\* +(\S+):[ ]+(\d{1,2})% \/(\d+.\d+)([a-zA-Z]+),", buffer
+        )
+        out = {}
+        processed, nb_files, progress = reg_checked[0]
+        out["progress"] = float(progress.strip())
+        out["processed"] = float(processed.strip())
+        out["nb_files"] = float(nb_files.strip())
         return True, out
 
     else:
@@ -182,19 +244,13 @@ def extract_rclone_progress(buffer: str) -> Tuple[bool, Union[Dict[str, Any], No
 
 
 def create_progress_bar(pbar_title: str) -> Tuple[Progress, TaskID]:
-    pbar = Progress(
-        TextColumn("[progress.description]{task.description}"),
-        SpinnerColumn(),
-        BarColumn(),
-        TaskProgressColumn(),
-        DownloadColumn(binary_units=True),
-        TimeRemainingColumn(),
-    )
+    pbar = MyProgress()
     pbar.start()
 
-    total_progress = pbar.add_task(pbar_title, total=None)
+    total_check_progress = pbar.add_task("Checking", total=None, progress_type="checking")
+    total_progress = pbar.add_task(pbar_title, total=None, progress_type="download")
 
-    return pbar, total_progress
+    return pbar, total_progress, total_check_progress
 
 
 def get_task(id: TaskID, progress: Progress) -> Task:
